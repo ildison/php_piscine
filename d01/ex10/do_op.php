186167b401e8e5ddc54c8dbab5da250aa5b5f1d7
ildison <ildarusmanov92@gmail.com>
2019-12-04 20:04:01 +0300

    fix do_op.php

--- d01/ex10/do_op.php
+++ d01/ex10/do_op.php
@@ -4,15 +4,17 @@
 if ($argc == 4)
 {
 	if (trim($argv[2]) == "+")
-		echo (trim($argv[1]) + trim($argv[3]))."\n";
+		echo (trim($argv[1]) + trim($argv[3])) . PHP_EOL;
 	else if (trim($argv[2]) == "-")
-		echo (trim($argv[1]) - trim($argv[3]))."\n";
+		echo (trim($argv[1]) - trim($argv[3])) . PHP_EOL;
 	else if (trim($argv[2]) == "*")
-		echo (trim($argv[1]) * trim($argv[3]))."\n";
+		echo (trim($argv[1]) * trim($argv[3])) . PHP_EOL;
 	else if (trim($argv[2]) == "/")
-		echo (trim($argv[1]) / trim($argv[3]))."\n";
+		echo (trim($argv[1]) / trim($argv[3])) . PHP_EOL;
 	else if (trim($argv[2]) == "%")
-		echo (trim($argv[1]) % trim($argv[3]))."\n";
+		echo (trim($argv[1]) % trim($argv[3])) . PHP_EOL;
 }
+else
+	echo "Incorrect Parameters" . PHP_EOL;
 
 ?>
